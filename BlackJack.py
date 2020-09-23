52bd210c8ed0df0e5b2b0f6d391776de4d185bb2
RoyBarel <48448944+RoyBarel@users.noreply.github.com>
2020-09-23 22:10:36 +0300

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -1,87 +1,117 @@
 import random
 
-CARDS=['2','3','4','5','6','7','8','9',"J","Q","K","A"]
-Dealer_cards= []
-Dealer_value_cards=[]
-Player_cards=[]
-Player_value_cards=[]
+
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
+              'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+playing = True
 
 
-def user_chice():
-    choice = ""
-    # validetion
+class Card:
+    def __init__(self,suit,rank):
+
+        self.suit=suit
+        self.rank=rank
+        #self.value = values[rank]
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+
+class Deck:
+    def __init__(self):
+        self.deck=[]
+
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+
+    def __str__(self):
+        deck_comp = ''  # start with an empty string
+        for card in self.deck:
+            deck_comp += '\n ' + card.__str__()  # add each Card object's print string
+        return 'The deck has:' + deck_comp
+
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        card = self.deck.pop()
+        return card
+
+
+class Hand:
+    def __init__(self):
+        self.cards=[]
+        self.value = 0
+        self.aces = 0    # add an attribute to keep track of aces
+
+
+    def __str__(self):
+        hand =''
+        for card in self.cards:
+            hand += "\n"+ card.__str__()
+        return "Hand cards has: "+hand
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        if card.rank =="Ace":
+            self.aces += 1
+
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.aces -= 1
+            self.value -= 10
+
+
+class Chips:
+    def __init__(self,total=100):
+        self.total=total# This can be set to a default value or supplied by a user input
+        self.bet =0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+
+def take_bet(chips):
+
     while True:
-        choice = input("Choose Hit or Stand \n"
-                       "h --> Hit\n"
-                       "s --> Stand\n")
-        if choice.lower() in ["s", "h"]:
-            return choice.lower()
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print("Please chose number")
+
         else:
-            print("you need to pick a choice from the options")
+            if chips.bet > self.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
 
-def print_cards(card_list,player):
-    print("Here is {} card:".format(player))
-    if player=="Dealer":
-        print(card_list[1:])
-    else:
-        print(card_list)
-
-def random_num():
-    return random.randint(0, 11)
-
-def card_type(random_index):
-    card = CARDS[random_index]
-    return card
-
-def value_of_card(card):
-    if card in ["j","Q","K"]:
-        return 10
-    if card == "A":
-        return 11
-    return int(card)
-
-def get_card():
-    return(card_type(random_num()))
-
-def total_cards_value(list_cards):
-    value =sum(list_cards)
-    if value >21:
-        if 11 in list_cards:
-            list_cards[list_cards.index(11)]=1
-            value =value -10
-
-    return value,list_cards
+def hit(deck, hand):
+    hand.add_card(deck.deal())
+    hand.adjust_for_ace()
 
 
-"""
-def dealer_gets_cards():
-    card = card_type(random_num())
-    value = value_of_card(card)
-    Dealer_cards.append(card)
-    Dealer_value_cards.append(value)
-    if sum(Dealer_value_cards)>21 and :
+def hit_or_stand(deck, hand):
+    global playing  # to control an upcoming while loop
+
+    pass
 
 
-"""
-###player cards
-for i in range(2):
-    card=get_card()
-    Player_cards.append(card)
-    Player_value_cards.append(value_of_card(card))
-
-###Dealer cards
-for i in range(2):
-    card = get_card()
-    Dealer_cards.append(card)
-    Dealer_value_cards.append(value_of_card(card))
-
-print_cards(Player_cards,"Player")
-total, Player_value_cards = total_cards_value(Player_value_cards)
-print(total,Player_value_cards)
-print_cards(Dealer_cards,"Dealer")
-total,Dealer_value_cards=total_cards_value(Dealer_value_cards)
-print(total,Dealer_value_cards)
-
-
-
-
+test_deck = Deck()
+test_deck.shuffle()
+test_player = Hand()
+test_player.add_card(test_deck.deal())
+test_player.add_card(test_deck.deal())
+test_player.add_card(test_deck.deal())
+print(test_player)
+print(test_player.value)
+take_bet()
